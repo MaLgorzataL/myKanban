10ed908e6f7b1bf3ce8e0d2e2f7ceaf203c81add
Malgorzata Lesniak <M999@interia.eu>
2019-03-29 13:26:16 +0100

    Start version with corrects

--- js/Column.js
+++ js/Column.js
@@ -1,18 +1,30 @@
-function Column(name) {
+function Column(id, name) {
   	var self = this;
-
-  	this.id = randomString();
-  	this.name = name;
+		this.id = id;
+    this.name = name || 'No name given';
   	this.element = generateTemplate('column-template', { name: this.name, id: this.id });
 
   	this.element.querySelector('.column').addEventListener('click', function (event) {
 	    if (event.target.classList.contains('btn-delete')) {
 	      	self.removeColumn();
 	    }
-	
-	    if (event.target.classList.contains('add-card')) {
-	      	self.addCard(new Card(prompt("Enter the name of the card")));
-	    }
+			if (event.target.classList.contains('add-card')) {
+				var cardName = prompt("Enter the name of the card");
+				event.preventDefault();
+				fetch(baseUrl + '/card', {
+					method: 'POST',
+					body: {
+						//body query
+					}
+				})
+				.then(function(res) {
+					return res.json();
+				})
+				.then(function() {
+					//create a new client side card
+				});
+				self.addCard(new Card(cardName));
+			}
 	});
 }
 
@@ -21,6 +33,13 @@ Column.prototype = {
 	  this.element.querySelector('ul').appendChild(card.element);
 	},
 	removeColumn: function() {
-	  this.element.parentNode.removeChild(this.element);
+		var self = this;
+		fetch(baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
+			.then(function(resp) {
+				return resp.json();
+			})
+			.then(function(resp) {
+				self.element.parentNode.removeChild(self.element);
+			});
 	}
 };
